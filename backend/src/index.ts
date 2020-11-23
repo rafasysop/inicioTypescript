acca65cda77ce1a7be2058b29d04342dbc3a45b7
rafasysop <rafaelcostamoura@gmail.com>
2020-11-23 07:16:10 -0300

    adicionando  ts-node-dev

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send('Hello World');
+    return res.send('Hello World jesus3');
 })
 
 app.listen(3333);
